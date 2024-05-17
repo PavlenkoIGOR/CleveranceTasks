24ab0d3877ad45684251e247fc0439f4b15a1b62
PavlenkoIGOR <illusiopard@gmail.com>
2024-05-17 21:02:10 +0300

    Добавьте файлы проекта.

---
+++ Task1/CompressingClass.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    internal static class CompressingClass
+    {
+        internal static string CompressedString(this string str)
+        {
+            char previousChar = str[0];
+            string compressedStr = "";
+
+            int countChar = 1;
+
+            for (int i = 1; i < str.Length; i++)
+            {
+                if (previousChar == str[i]) //if the chars are repeats...
+                {
+                    countChar++;
+                }
+                else //if not
+                {
+                    compressedStr += previousChar;
+
+                    if (countChar > 1)
+                        compressedStr += countChar;
+
+                    previousChar = str[i];
+                    countChar = 1;
+                }
+            }
+
+            //for last type of the char
+            compressedStr += previousChar;
+            if (countChar > 1)
+            {
+                compressedStr += countChar;
+            }
+            return compressedStr;
+        }
+    }
+}
---
+++ Task1/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Task1;
+
+
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Enter a string:");
+//string strForCompress = Console.ReadLine();
+
+string compressedString = Console.ReadLine().CompressedString();
+Console.WriteLine($"Compressed string: {compressedString}");
+Console.ReadLine();
+
+
