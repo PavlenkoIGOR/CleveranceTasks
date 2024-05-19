94269ae40732892eb861165c61a6d1ae2a52ed62
PavlenkoIGOR <illusiopard@gmail.com>
2024-05-19 13:51:43 +0300

    program Task3 has been changed

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -4,8 +4,25 @@
     {
         static void Main(string[] args)
         {
-            Thread readingThread = new Thread(async ()=>await PsevdoServer.GetCount());
-            readingThread.Start();
+            Console.WriteLine("Симуляция обращений к серверу...");
+
+            // Запуск нескольких потоков, имитирующих разных пользователей
+            var threads = new[] {
+                new Thread(() => { PsevdoServer.AddToCount(10); }),
+                new Thread(() => { PsevdoServer.GetCount(); }),
+                new Thread(() => { PsevdoServer.AddToCount(20); }),
+                new Thread(() => { PsevdoServer.GetCount(); })
+            };
+
+            foreach (var thread in threads)
+            {
+                thread.Start();
+            }
+
+            foreach (var thread in threads)
+            {
+                thread.Join();
+            }
             Console.ReadLine();
         }
     }
